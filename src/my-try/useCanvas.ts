b5750692a8dd6698c515e4e245b684df90291d05
codesthenos <codesthenos@gmail.com>
2024-10-09 20:58:55 +0200

    Mi forma de hacerlo va cogiendo ya colorsito, ya tengo una caja moviendose, y cuando aprieto espacio o el click se para y aparece otra justo arriba y asi sucesivamente, quedan cositas todavia, que se vaya disminuyendo el ancho de la caja siguiente, que si con coincide nada se pause y muestre un game over, que la camara vaya subiendo y el scroll moviendose adecuadamente, que cambien de color que la velocidad vay aumentando...

--- src/my-try/useCanvas.ts
+++ src/my-try/useCanvas.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import type { box } from './types.d.ts'
-import { INITIAL_BOX, INITIAL_X_SPEED, MODE } from './constants.ts'
+import { BOX_HEIGHT, CANVAS_WIDTH, INITIAL_BOX, INITIAL_BOX_X, INITIAL_X_SPEED, MODE } from './constants.ts'
 
 function useCanvas (draw: ({ context, boxes, mode }: { context: CanvasRenderingContext2D, boxes: box[], mode: MODE }) => void) {
 
@@ -9,6 +9,7 @@ function useCanvas (draw: ({ context, boxes, mode }: { context: CanvasRenderingC
   const speedRef = useRef(INITIAL_X_SPEED)
   const boxesRef = useRef<box[]>([INITIAL_BOX])
   const modeRef = useRef<MODE>(MODE.BOUNCE)
+  const currentRef = useRef(0)
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -16,18 +17,34 @@ function useCanvas (draw: ({ context, boxes, mode }: { context: CanvasRenderingC
     const context = canvas.getContext('2d')
     
     function moveBox () {
-      if ((speedRef.current > 0 && boxesRef.current[0].x > 350) || (speedRef.current < 0 && boxesRef.current[0].x < -50) ) {
+      if ((speedRef.current > 0 && boxesRef.current[currentRef.current].x > CANVAS_WIDTH - 50) || (speedRef.current < 0 && boxesRef.current[currentRef.current].x < -50) ) {
         speedRef.current = -speedRef.current
       }
       
-      boxesRef.current[0].x += speedRef.current
+      boxesRef.current[currentRef.current].x += speedRef.current
+    }
+
+    function addBox () {
+      const newBox = {
+        x: INITIAL_BOX_X,
+        y: boxesRef.current[currentRef.current].y + BOX_HEIGHT,
+        width: boxesRef.current[currentRef.current].width,
+        color: "#eee"
+      }
+      boxesRef.current = [...boxesRef.current, newBox]
     }
 
     function render () {
       if (!context) return
 
       //Start Gameloop
-      if (modeRef.current === MODE.BOUNCE) moveBox()
+      if (modeRef.current === MODE.BOUNCE) {
+        moveBox()
+      } else if (modeRef.current === MODE.STOP) {
+        addBox()
+        modeRef.current = MODE.BOUNCE
+        currentRef.current++
+      }
       draw({ context, boxes: boxesRef.current, mode: modeRef.current })
       frameIdRef.current = window.requestAnimationFrame(render)
     }
@@ -35,7 +52,6 @@ function useCanvas (draw: ({ context, boxes, mode }: { context: CanvasRenderingC
     
     function handleInput () {
       if (modeRef.current === MODE.BOUNCE) modeRef.current = MODE.STOP
-      else if (modeRef.current === MODE.STOP) modeRef.current = MODE.BOUNCE
     }
 
     canvas.addEventListener('pointerdown', handleInput)
